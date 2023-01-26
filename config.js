56802b4c891aba140bc5be3bfe60ed5a1d5be353
hacnguyen7it <nguyenhohoanghac7it@gmail.com>
2023-01-26 15:06:41 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Quà tết của Thiên Phú🎁',
+    introDesc: `Anh tặng em món quà có một chút đặt biệt bấm tiếp đuy kkk`,
+    btnIntro: 'Okela',
+    title: 'Anh biết em thích nhận quà lắm lắm luôn phải hong ? 😜',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em hong được nhận quà đó nghen 😎',
+    btnYes: 'Có, em thích quà này lắm luôn 🥹',
+    btnNo: 'Thôi, này rườm rà quá! 🥱',
+    question:'Gõ một vài lời gì đó để cảm ơn anh đi 😝😝',
+    reasonPlaceholder: 'Khen hoặc chê anh gì anh cũng chịu hết',
+    btnReply: 'Gửi💋💌',
+    reply: 'Anh thiệc là đẹp troaiii quá đi Ahiiiii😍',
+    mess: 'Anh biết mà, Anh biết thế nào cũng khen anh mà 🥰. Không có tiền tặng mắc cài đâu 🤪',
+    messDesc: 'Nhấn Tiếp để thấy được món quà hen 😸',
+    btnAccept: 'Dạ. *TIếng lòng: "Lẹ đi em muốn thấy lắm rồi" ',
+    messLink: 'https://qrco.de/quatetcuaaido'
+}
